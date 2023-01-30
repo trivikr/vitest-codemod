91f4134bbc34ef44b087fcb45a17da3bc49a2315
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-29 19:54:23 -0800

    Remove redundant transform.ts

--- packages/jest/src/index.ts
+++ packages/jest/src/index.ts
@@ -1,1 +1,6 @@
-export * from "./transform";
+import { VitestCodemodTransform } from "@vitest-codemod/types";
+
+export const transform: VitestCodemodTransform = {
+  name: "jest",
+  description: "Converts Jest APIs in a Javascript/TypeScript codebase to Vitest",
+}
--- packages/jest/src/transform.ts
+++
@@ -1,6 +0,0 @@
-import { VitestCodemodTransform } from "@vitest-codemod/types";
-
-export const transform: VitestCodemodTransform = {
-  name: "jest",
-  description: "Converts Jest APIs in a Javascript/TypeScript codebase to Vitest",
-}
