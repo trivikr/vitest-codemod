ff3363902ffa4b4ceec689ab1681332cbe310a90
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-29 17:00:50 -0800

    Add a ToDo to return transforms from @vitest-codemod/* packages

--- packages/vitest-codemod/src/utils/getTransforms.ts
+++ packages/vitest-codemod/src/utils/getTransforms.ts
@@ -1,13 +1,4 @@
-import { readdirSync } from "fs";
-import { join } from "path";
+// import { VitestCodemodTransform } from "@vitest-codemod/types";
 
-import { VitestCodemodTransform } from "../transforms";
-
-export const getTransforms = () =>
-  readdirSync(join(__dirname, "..", "transforms"), { withFileTypes: true })
-    .filter((dirent) => dirent.isDirectory())
-    .map((dirent) => dirent.name)
-    .map((dirName) => {
-      const { name, description, options } = require(`../transforms/${dirName}/transform`); // eslint-disable-line
-      return { name, description, options } as VitestCodemodTransform;
-    });
+// ToDo: Return transforms from @vitest-codemod/* packages
+export const getTransforms = () => [];
