79385bd5436b014733240a732eed665b6145598a
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-30 21:18:54 -0800

    Use recast defaults in input/output

--- packages/jest/src/__fixtures__/basic.output.js
+++ packages/jest/src/__fixtures__/basic.output.js
@@ -1,9 +1,8 @@
-import { describe, expect, test } from 'vitest'
-
-describe('basic', () => {
-  test('Math.sqrt()', () => {
-    expect(Math.sqrt(4)).toBe(2)
-    expect(Math.sqrt(144)).toBe(12)
-    expect(Math.sqrt(2)).toBe(Math.SQRT2)
+import { describe, expect, test } from "vitest";
+describe("basic", () => {
+  test("Math.sqrt()", () => {
+    expect(Math.sqrt(4)).toBe(2);
+    expect(Math.sqrt(144)).toBe(12);
+    expect(Math.sqrt(2)).toBe(Math.SQRT2);
   })
 })
