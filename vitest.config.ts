af20cc7aed9a6691aac275c4da19fd8010b70e66
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-30 20:36:38 -0800

    Force vitest rerun when files are __fixtures__ are edited

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     include: ['**/src/__fixtures__/*.output.{js,ts}', '**/*.spec.ts'],
+    forceRerunTriggers: ['**/__fixtures__/**'],
   },
 })
