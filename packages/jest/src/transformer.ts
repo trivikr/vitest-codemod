a00b91fb45e9b847cca2bef0f7d8e603d20d63ac
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-30 21:17:20 -0800

    Add transform code

--- packages/jest/src/transformer.ts
+++ packages/jest/src/transformer.ts
@@ -1,10 +1,23 @@
 import type { API, FileInfo } from 'jscodeshift'
+import addImports from 'jscodeshift-add-imports'
 
 const transformer = async (file: FileInfo, api: API) => {
   const j = api.jscodeshift
   const source = j(file.source)
 
-  // ToDo: Add the transform logic here
+  const apis = []
+
+  for (const name of ['afterAll', 'afterEach', 'beforeAll', 'beforeEach', 'describe', 'test', 'it', 'expect']) {
+    const calls = source.find(j.CallExpression, { callee: { name } })
+
+    if (calls.length > 0)
+      apis.push(j.importSpecifier(j.identifier(name), j.identifier(name)))
+  }
+
+  if (apis.length) {
+    apis.sort()
+    addImports(source, j.importDeclaration(apis, j.stringLiteral('vitest')))
+  }
 
   return source.toSource()
 }
