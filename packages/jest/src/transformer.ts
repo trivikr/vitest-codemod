9a534e3bfc0491886cb752b46e769ea9970af272
Trivikram Kamat <16024985+trivikr@users.noreply.github.com>
2023-01-30 21:47:15 -0800

    Add transform for vitest imports for jest globals (#42)

--- packages/jest/src/transformer.ts
+++ packages/jest/src/transformer.ts
@@ -4,7 +4,21 @@ const transformer = async (file: FileInfo, api: API) => {
   const j = api.jscodeshift
   const source = j(file.source)
 
-  // ToDo: Add the transform logic here
+  const apis = []
+
+  for (const name of ['afterAll', 'afterEach', 'beforeAll', 'beforeEach', 'describe', 'test', 'it', 'expect']) {
+    const calls = source.find(j.CallExpression, { callee: { name } })
+
+    if (calls.length > 0)
+      apis.push(name)
+  }
+
+  if (apis.length) {
+    apis.sort()
+    const importSpecifiers = apis.map(apiName => j.importSpecifier(j.identifier(apiName)))
+    const importDeclaration = j.importDeclaration(importSpecifiers, j.stringLiteral('vitest'))
+    source.get().node.program.body.unshift(importDeclaration)
+  }
 
   return source.toSource()
 }
