1af290f012ba277cfe66475c433edcfea27acc16
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-31 21:29:07 -0800

    Add test-only

---
+++ packages/jest/src/__fixtures__/test-only.input.js
@@ -0,0 +1,27 @@
+describe("test-skip", () => {
+  // This failing test won't be run
+  it("Math.sqrt(4)", () => {
+    expect(Math.sqrt(4)).toBe(3);
+  });
+  it.only("Math.sqrt(4)", () => {
+    expect(Math.sqrt(4)).toBe(2);
+  });
+  it.only.each([
+    [[4, 2],[9, 3]]
+  ])("Math.sqrt(%s) = %s", ([input, output]) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
+
+  // This failing test won't be run
+  test("Math.sqrt(4)", () => {
+    expect(Math.sqrt(4)).toBe(3);
+  });
+  test.only("Math.sqrt(4)", () => {
+    expect(Math.sqrt(4)).toBe(2);
+  });
+  test.only.each([
+    [[4, 2],[9, 3]]
+  ])("Math.sqrt(%s) = %s", ([input, output]) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
+});
