2ba0f1726e788092f39531540df24dece6ca2add
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-31 22:20:20 -0800

    Add test-concurrent-each

---
+++ packages/jest/src/__fixtures__/test/test-concurrent-each.input.js
@@ -0,0 +1,15 @@
+describe("test-each", () => {
+  it.concurrent.each([
+    [4, 2],
+    [9, 3],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
+
+  test.concurrent.each([
+    [4, 2],
+    [9, 3],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
+});
