b4caf941c5f572d2ad13f0e1a92aa817af940e81
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-01-31 18:54:49 -0800

    Add transform to process test members

--- packages/jest/src/transformer.ts
+++ packages/jest/src/transformer.ts
@@ -1,21 +1,44 @@
-import type { API, FileInfo } from 'jscodeshift'
+import type { API, FileInfo, Identifier } from 'jscodeshift'
+
+const jestGlobalApis = ['afterAll', 'afterEach', 'beforeAll', 'beforeEach', 'describe', 'test', 'it', 'expect']
+
+const testApiProps = ['concurrent', 'each', 'only', 'skip', 'todo']
+const jestGlobalApiProps = {
+  it: testApiProps,
+  test: testApiProps,
+}
 
 const transformer = async (file: FileInfo, api: API) => {
   const j = api.jscodeshift
   const source = j(file.source)
 
-  const apis = []
+  const vitestApis = []
 
-  for (const name of ['afterAll', 'afterEach', 'beforeAll', 'beforeEach', 'describe', 'test', 'it', 'expect']) {
-    const calls = source.find(j.CallExpression, { callee: { name } })
+  for (const jestGlobalApi of jestGlobalApis) {
+    const calls = source.find(j.CallExpression, { callee: { name: jestGlobalApi } })
 
-    if (calls.length > 0)
-      apis.push(name)
+    if (calls.length > 0) {
+      vitestApis.push(jestGlobalApi)
+    }
+    else if (Object.keys(jestGlobalApiProps).includes(jestGlobalApi)) {
+      const propNamesList = source.find(j.MemberExpression, {
+        object: { name: jestGlobalApi },
+        property: { type: 'Identifier' },
+      }).nodes().map(node => (node.property as Identifier).name)
+
+      const propNames = [...new Set(propNamesList)]
+      for (const propName of propNames) {
+        if (jestGlobalApiProps[jestGlobalApi].includes(propName)) {
+          vitestApis.push(jestGlobalApi)
+          break
+        }
+      }
+    }
   }
 
-  if (apis.length) {
-    apis.sort()
-    const importSpecifiers = apis.map(apiName => j.importSpecifier(j.identifier(apiName)))
+  if (vitestApis.length) {
+    vitestApis.sort()
+    const importSpecifiers = vitestApis.map(apiName => j.importSpecifier(j.identifier(apiName)))
     const importDeclaration = j.importDeclaration(importSpecifiers, j.stringLiteral('vitest'))
     source.get('program', 'body').unshift(importDeclaration)
   }
