be129a43d2ed23482724ee1cbb3dd59367641b19
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 19:49:35 -0800

    Add test fit-each

--- packages/jest/src/__fixtures__/test/fit.input.js
+++ packages/jest/src/__fixtures__/test/fit.input.js
@@ -2,11 +2,4 @@ describe("fit", () => {
   fit("Math.sqrt(4)", () => {
     expect(Math.sqrt(4)).toBe(2);
   });
-
-  fit.each([
-    [4, 2],
-    [9, 3],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
-  });
 });
--- packages/jest/src/__fixtures__/test/fit.output.js
+++ packages/jest/src/__fixtures__/test/fit.output.js
@@ -3,11 +3,4 @@ describe("fit", () => {
   it.only("Math.sqrt(4)", () => {
     expect(Math.sqrt(4)).toBe(2);
   });
-
-  it.only.each([
-    [4, 2],
-    [9, 3],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
-  });
 });
