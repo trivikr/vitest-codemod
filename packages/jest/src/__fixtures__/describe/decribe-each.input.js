f87fb6dd3b8604ce74b2dad82f6b2797cb6614b7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 20:23:50 -0800

    Add test describe-each

---
+++ packages/jest/src/__fixtures__/describe/decribe-each.input.js
@@ -0,0 +1,17 @@
+describe.each([
+  [1, 1, 2],
+  [1, 2, 3],
+  [2, 1, 3],
+])('.add(%i, %i)', (a, b, expected) => {
+  test(`returns ${expected}`, () => {
+    expect(a + b).toBe(expected);
+  });
+
+  test(`returned value not be greater than ${expected}`, () => {
+    expect(a + b).not.toBeGreaterThan(expected);
+  });
+
+  test(`returned value not be less than ${expected}`, () => {
+    expect(a + b).not.toBeLessThan(expected);
+  });
+});
