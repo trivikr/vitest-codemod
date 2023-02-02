02019dcdb64b4770d5f3b02f612a178608d0185c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 20:37:06 -0800

    Add test describe-only-each

---
+++ packages/jest/src/__fixtures__/describe/describe-only-each.output.js
@@ -0,0 +1,14 @@
+import { describe, expect, test } from "vitest";
+describe.only.each([
+  [1, 1, 2],
+  [1, 2, 3],
+  [2, 1, 3],
+])('.add(%i, %i)', (a, b, expected) => {
+  test(`returns ${expected}`, () => {
+    expect(a + b).toBe(expected);
+  });
+});
+
+test('will not be run', () => {
+  expect(1 / 0).toBe(Infinity);
+});
