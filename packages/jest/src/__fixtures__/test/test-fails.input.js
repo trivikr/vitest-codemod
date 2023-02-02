75049602640950d7217415648f438b8bda564b69
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 17:54:19 -0800

    Add test.fails.each in test-fails

--- packages/jest/src/__fixtures__/test/test-fails.input.js
+++ packages/jest/src/__fixtures__/test/test-fails.input.js
@@ -2,8 +2,20 @@ describe("test-member", () => {
   it.failing("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
   });
+  it.failing.each([
+    [4, 3],
+    [9, 4],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
 
   test.failing("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
+  });
+  test.failing.each([
+    [4, 3],
+    [9, 4],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
   });
 });
--- packages/jest/src/__fixtures__/test/test-fails.output.js
+++ packages/jest/src/__fixtures__/test/test-fails.output.js
@@ -3,8 +3,20 @@ describe("test-member", () => {
   it.fails("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
   });
+  it.fails.each([
+    [4, 3],
+    [9, 4],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
+  });
 
   test.fails("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
+  });
+  test.fails.each([
+    [4, 3],
+    [9, 4],
+  ])("Math.sqrt(%s) = %s", (input, output) => {
+    expect(Math.sqrt(input)).toBe(output);
   });
 });
