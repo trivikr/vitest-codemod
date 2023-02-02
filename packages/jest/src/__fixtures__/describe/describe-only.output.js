cc3b4cbf35b5afa9163fbc9d67a09d5a9677b560
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 20:41:48 -0800

    Add test which won't be run in describe-only

--- packages/jest/src/__fixtures__/describe/describe-only.output.js
+++ packages/jest/src/__fixtures__/describe/describe-only.output.js
@@ -3,4 +3,8 @@ describe.only("describe-only", () => {
   test("Math.sqrt()", () => {
     expect(Math.sqrt(4)).toBe(2);
   })
-})
+});
+
+test('will not be run', () => {
+  expect(Math.sqrt(4)).toBe(3);
+});
