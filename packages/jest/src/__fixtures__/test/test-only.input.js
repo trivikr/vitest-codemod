4db4dfa165e0b4a7402188ca00e3854aa78ba119
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 20:01:58 -0800

    Add test test-only-each

--- packages/jest/src/__fixtures__/test/test-only.input.js
+++ packages/jest/src/__fixtures__/test/test-only.input.js
@@ -2,20 +2,8 @@ describe("test-only", () => {
   it.only("Math.sqrt(4)", () => {
     expect(Math.sqrt(4)).toBe(2);
   });
-  it.only.each([
-    [4, 2],
-    [9, 3],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
-  });
 
   test.only("Math.sqrt(4)", () => {
     expect(Math.sqrt(4)).toBe(2);
-  });
-  test.only.each([
-    [4, 2],
-    [9, 3],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
   });
 });
