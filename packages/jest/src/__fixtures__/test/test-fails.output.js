864e1813bbab9a5bf370a773edab7151b7f2e856
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-01 19:57:55 -0800

    Add test test-fails-each

--- packages/jest/src/__fixtures__/test/test-fails.output.js
+++ packages/jest/src/__fixtures__/test/test-fails.output.js
@@ -3,20 +3,8 @@ describe("test-fails", () => {
   it.fails("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
   });
-  it.fails.each([
-    [4, 3],
-    [9, 4],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
-  });
 
   test.fails("Math.sqrt(4) = 3", () => {
     expect(Math.sqrt(4)).toBe(3);
-  });
-  test.fails.each([
-    [4, 3],
-    [9, 4],
-  ])("Math.sqrt(%s) = %s", (input, output) => {
-    expect(Math.sqrt(input)).toBe(output);
   });
 });
