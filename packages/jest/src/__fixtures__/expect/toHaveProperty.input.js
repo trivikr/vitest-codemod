8f899396bedf5e959a8ac6ebd0054cd099d2cec8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-03 17:18:25 -0800

    Add test expect/toHaveProperty

---
+++ packages/jest/src/__fixtures__/expect/toHaveProperty.input.js
@@ -0,0 +1,57 @@
+// Object containing house features to be tested
+const houseForSale = {
+  bath: true,
+  bedrooms: 4,
+  kitchen: {
+    amenities: ["oven", "stove", "washer"],
+    area: 20,
+    wallColor: "white",
+    "nice.oven": true,
+  },
+  livingroom: {
+    amenities: [
+      {
+        couch: [
+          ["large", { dimensions: [20, 20] }],
+          ["small", { dimensions: [10, 10] }],
+        ],
+      },
+    ],
+  },
+  "ceiling.height": "tall",
+};
+
+test("this house has my desired features", () => {
+  // Example Referencing
+  expect(houseForSale).toHaveProperty("bath");
+  expect(houseForSale).toHaveProperty("bedrooms", 4);
+
+  expect(houseForSale).not.toHaveProperty("pool");
+
+  // Deep referencing using dot notation
+  expect(houseForSale).toHaveProperty("kitchen.area", 20);
+  expect(houseForSale).toHaveProperty("kitchen.amenities", [
+    "oven",
+    "stove",
+    "washer",
+  ]);
+
+  expect(houseForSale).not.toHaveProperty("kitchen.open");
+
+  // Deep referencing using an array containing the keyPath
+  expect(houseForSale).toHaveProperty(["kitchen", "area"], 20);
+  expect(houseForSale).toHaveProperty(
+    ["kitchen", "amenities"],
+    ["oven", "stove", "washer"]
+  );
+  expect(houseForSale).toHaveProperty(["kitchen", "amenities", 0], "oven");
+  expect(houseForSale).toHaveProperty(
+    "livingroom.amenities[0].couch[0][1].dimensions[0]",
+    20
+  );
+  expect(houseForSale).toHaveProperty(["kitchen", "nice.oven"]);
+  expect(houseForSale).not.toHaveProperty(["kitchen", "open"]);
+
+  // Referencing keys with dot in the key itself
+  expect(houseForSale).toHaveProperty(["ceiling.height"], "tall");
+});
