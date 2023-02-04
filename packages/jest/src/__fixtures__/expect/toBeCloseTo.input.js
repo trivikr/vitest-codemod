135bbc637714cd6f05927383b26f845988bb6e90
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-04 11:34:40 -0800

    Add test expect/toBeCloseTo

---
+++ packages/jest/src/__fixtures__/expect/toBeCloseTo.input.js
@@ -0,0 +1,10 @@
+test.failing("decimals are not equal in javascript", () => {
+  expect(0.2 + 0.1).toBe(0.3); // 0.2 + 0.1 is 0.30000000000000004
+});
+
+test("decimals are rounded to 5 after the point", () => {
+  // 0.2 + 0.1 is 0.30000 | "000000000004" removed
+  expect(0.2 + 0.1).toBeCloseTo(0.3, 5);
+  // nothing from 0.30000000000000004 is removed
+  expect(0.2 + 0.1).not.toBeCloseTo(0.3, 50);
+});
