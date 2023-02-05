062b39af4cad47dac341f62bce4e5de410a0a507
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-05 12:11:23 -0800

    Add test mock/instances

---
+++ packages/jest/src/__fixtures__/mock/instances.output.js
@@ -0,0 +1,13 @@
+import { expect, test, vi } from "vitest";
+test("instances", () => {
+  const mockFn = vi.fn();
+  expect(mockFn.mock.instances).toHaveLength(0);
+
+  const a = new mockFn();
+  expect(mockFn.mock.instances).toHaveLength(1);
+  expect(mockFn.mock.instances[0]).toBe(a);
+
+  const b = new mockFn();
+  expect(mockFn.mock.instances).toHaveLength(2);
+  expect(mockFn.mock.instances[1]).toBe(b);
+});
