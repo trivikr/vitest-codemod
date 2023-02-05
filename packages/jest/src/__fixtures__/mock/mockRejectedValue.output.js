24d2d18ff1b5ae4f30d441dc07bfa7046e021166
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-05 15:29:39 -0800

    Add test mock/mockRejectedValue

---
+++ packages/jest/src/__fixtures__/mock/mockRejectedValue.output.js
@@ -0,0 +1,12 @@
+import { expect, test, vi } from "vitest";
+test("mockRejectedValue", () => {
+  const originalRejectMsg = "originalRejectMsg";
+  const updatedRejectMsg = "updatedRejectMsg";
+
+  const mockFn = vi.fn(() => Promise.reject(originalRejectMsg));
+  expect(mockFn()).rejects.toBe(originalRejectMsg);
+  
+  mockFn.mockRejectedValue(updatedRejectMsg);
+  expect(mockFn()).rejects.toBe(updatedRejectMsg);
+  expect(mockFn()).rejects.toBe(updatedRejectMsg);
+});
