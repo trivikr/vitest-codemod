b68ce8db7272550b3eb62e114fbcaad306ec8a16
John Gozde <john@gozde.ca>
2023-02-05 10:15:47 -0700

    Add test for existing imports

---
+++ packages/jest/src/__fixtures__/misc/with-existing-imports.input.mjs
@@ -0,0 +1,9 @@
+import assert from 'node:assert';
+
+describe("basic", () => {
+  test("Math.sqrt()", () => {
+    assert(Math.sqrt(4) === 2);
+    assert(Math.sqrt(144) === 12);
+    assert(Math.sqrt(2) === Math.SQRT2);
+  })
+})
