fd2ab55ba16e37a7d468907659536fb4e31cf623
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-05 15:21:01 -0800

    Add test mock/mockReturnValueOnce

---
+++ packages/jest/src/__fixtures__/mock/mockReturnValueOnce.input.js
@@ -0,0 +1,9 @@
+test("mockImplementation", () => {
+  const mockFn = jest.fn(() => 1);
+  expect(mockFn()).toBe(1);
+  
+  mockFn.mockReturnValueOnce(42);
+  expect(mockFn()).toBe(42);
+
+  expect(mockFn()).toBe(1);
+});
