d6295f6f53d46d6f540e93f21437cf2639f0d745
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-05 00:50:39 -0800

    Add test expect/toStrictEqual

---
+++ packages/jest/src/__fixtures__/expect/toStrictEqual.output.js
@@ -0,0 +1,11 @@
+import { expect, test } from "vitest";
+class Stock {
+  constructor(type) {
+    this.type = type;
+  }
+}
+
+test("structurally the same, but semantically different", () => {
+  expect(new Stock("apples")).toEqual({ type: "apples" });
+  expect(new Stock("apples")).not.toStrictEqual({ type: "apples" });
+});
