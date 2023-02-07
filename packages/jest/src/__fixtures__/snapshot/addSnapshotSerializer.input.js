01a40e94c865a6135d3a7d61b24faf72268d731c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-07 15:44:57 -0800

    Add test snapshot/addSnapshotSerializer

---
+++ packages/jest/src/__fixtures__/snapshot/addSnapshotSerializer.input.js
@@ -0,0 +1,30 @@
+describe("addSnapshotSerializer", () => {
+  expect.addSnapshotSerializer({
+    serialize(val) {
+      return JSON.stringify(val);
+    },
+    test(val) {
+      return val;
+    }
+  });
+
+  describe("toMatchSnapshot", () => {
+    test("string", () => {
+      expect("foobar".toUpperCase()).toMatchSnapshot();
+    });
+
+    test("object", () => {
+      expect({ foo: "bar" }).toMatchSnapshot();
+    });
+  });
+
+  describe("toMatchInlineSnapshot", () => {
+    test("string", () => {
+      expect("foobar".toUpperCase()).toMatchInlineSnapshot(`"FOOBAR"`);
+    });
+
+    test("object", () => {
+      expect({ foo: "bar" }).toMatchInlineSnapshot(`{"foo":"bar"}`);
+    });
+  });
+});
