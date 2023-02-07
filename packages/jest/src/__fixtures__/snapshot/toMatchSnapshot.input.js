85862d19d45cb612908479fdc56dbd9784e304b4
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-07 15:25:48 -0800

    Add object with expect.any in snapshot/toMatchSnapshot

--- packages/jest/src/__fixtures__/snapshot/toMatchSnapshot.input.js
+++ packages/jest/src/__fixtures__/snapshot/toMatchSnapshot.input.js
@@ -1,3 +1,16 @@
-it("toMatchSnapshot", () => {
-  expect("foobar".toUpperCase()).toMatchSnapshot();
+describe("toMatchSnapshot", () => {
+  test("string", () => {
+    expect("foobar".toUpperCase()).toMatchSnapshot();
+  });
+
+  test("object", () => {
+    expect({
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+      name: "John Doe"
+    }).toMatchSnapshot({
+      createdAt: expect.any(Date),
+      id: expect.any(Number),
+    });
+  });
 });
--- packages/jest/src/__fixtures__/snapshot/toMatchSnapshot.output.js
+++ packages/jest/src/__fixtures__/snapshot/toMatchSnapshot.output.js
@@ -1,4 +1,17 @@
-import { expect, it } from "vitest";
-it("toMatchSnapshot", () => {
-  expect("foobar".toUpperCase()).toMatchSnapshot();
+import { describe, expect, test } from "vitest";
+describe("toMatchSnapshot", () => {
+  test("string", () => {
+    expect("foobar".toUpperCase()).toMatchSnapshot();
+  });
+
+  test("object", () => {
+    expect({
+      createdAt: new Date(),
+      id: Math.floor(Math.random() * 20),
+      name: "John Doe"
+    }).toMatchSnapshot({
+      createdAt: expect.any(Date),
+      id: expect.any(Number),
+    });
+  });
 });
