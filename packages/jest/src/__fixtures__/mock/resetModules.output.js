6871cf8cd270f422c4f53c595e4d30f9ce82fe0c
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-07 18:05:03 -0800

    Add test mock/resetModules

---
+++ packages/jest/src/__fixtures__/mock/resetModules.output.js
@@ -0,0 +1,12 @@
+import { expect, test, vi } from "vitest";
+test("resetModules", async () => {
+  const { getToken: getToken1 } = await import("./getToken");
+  const { getToken: getToken2 } = await import("./getToken");
+  expect(getToken1).toBe(getToken2);
+  
+  vi.resetModules();
+  
+  const { getToken: getToken3 } = await import("./getToken");
+  expect(getToken1).not.toBe(getToken3);
+  expect(getToken2).not.toBe(getToken3);
+});
