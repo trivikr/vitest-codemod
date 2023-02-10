d88badc216a0708aa5e5c11619daef751f031f7b
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-09 22:42:41 -0800

    Add apiNamesRecord in replaceJestObjectWithVi

--- packages/jest/src/apis/replaceJestObjectWithVi.ts
+++ packages/jest/src/apis/replaceJestObjectWithVi.ts
@@ -1,21 +1,28 @@
 import type { Collection, JSCodeshift } from 'jscodeshift'
 
 const unavailableAutomockApis = ['disableAutomock', 'enableAutomock']
+const apiNamesRecord: Record<string, string> = {
+  createMockFromModule: 'importMock',
+}
 
 export const replaceJestObjectWithVi = (j: JSCodeshift, source: Collection<any>): void => {
   // Replace `jest` with `vi`
   source.find(j.MemberExpression, {
     object: { type: 'Identifier', name: 'jest' },
   }).forEach((path) => {
-    if (
-      path.node.property.type === 'Identifier'
-      && unavailableAutomockApis.includes(path.node.property.name)
-    ) {
-      throw new Error(
-        `The automocking API "${path.node.property.name}" is not supported in vitest.\n\n`
-        + 'Please switch to explicit mocking in Jest before running transformation, or '
-        + 'skip transformation on the files which uses this API.',
-      )
+    if (path.node.property.type === 'Identifier') {
+      const propertyName = path.node.property.name
+
+      if (unavailableAutomockApis.includes(propertyName)) {
+        throw new Error(
+          `The automocking API "${propertyName}" is not supported in vitest.\n\n`
+          + 'Please switch to explicit mocking in Jest before running transformation, or '
+          + 'skip transformation on the files which uses this API.',
+        )
+      }
+
+      if (apiNamesRecord[propertyName])
+        path.node.property.name = apiNamesRecord[propertyName]
     }
 
     path.node.object = j.identifier('vi')
