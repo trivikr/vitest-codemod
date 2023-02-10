bafa7c865be805e6f446c5c9cee15be4369e7b8e
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-09 16:58:07 -0800

    Add 'not' supported in error

--- packages/jest/src/apis/replaceJestObjectWithVi.ts
+++ packages/jest/src/apis/replaceJestObjectWithVi.ts
@@ -12,7 +12,7 @@ export const replaceJestObjectWithVi = (j: JSCodeshift, source: Collection<any>)
       && unavailableAutomockApis.includes(path.node.property.name)
     ) {
       throw new Error(
-        `The automocking API "${path.node.property.name}" is supported in vitest.\n\n`
+        `The automocking API "${path.node.property.name}" is not supported in vitest.\n\n`
         + 'Please switch to explicit mocking in Jest before running transformation, or'
         + 'skip transformation on the files which uses this API.',
       )
