329ba90d13333d30df5eb96f4578a66ab59d426f
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-11 13:10:23 -0800

    Add test expect/toThrowError

---
+++ packages/jest/src/__fixtures__/expect/toThrowError.input.js
@@ -0,0 +1,23 @@
+test("toThrowError", () => {
+  class CustomError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "CustomError";
+    }
+  }
+
+  function throwFn() {
+    throw new CustomError("foo bar baz");
+  };
+  
+  // Test that the error message says "foo" somewhere: these are equivalent
+  expect(throwFn).toThrowError(/foo/);
+  expect(throwFn).toThrowError("foo");
+
+  // Test the exact error message
+  expect(throwFn).toThrowError(/^foo bar baz$/);
+  expect(throwFn).toThrowError(new Error("foo bar baz"));
+
+  // Test that we get a DisgustingFlavorError
+  expect(throwFn).toThrowError(CustomError);
+});
