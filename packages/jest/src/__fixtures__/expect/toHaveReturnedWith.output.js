7eb5246a708acbda4d080c87146ad0f4fc3db727
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-11 05:08:11 -0800

    Add test case toReturnWith('bar')

--- packages/jest/src/__fixtures__/expect/toHaveReturnedWith.output.js
+++ packages/jest/src/__fixtures__/expect/toHaveReturnedWith.output.js
@@ -2,5 +2,7 @@ import { expect, test, vi } from "vitest";
 test("toHaveReturnedWith", () => {
   const mockFn = vi.fn(input => `mockFn(${input})`);
   mockFn("foo");
+  mockFn("bar");
   expect(mockFn).toHaveReturnedWith(`mockFn(foo)`);
+  expect(mockFn).toHaveReturnedWith(`mockFn(bar)`);
 });
--- packages/jest/src/__fixtures__/expect/toReturnWith.output.js
+++ packages/jest/src/__fixtures__/expect/toReturnWith.output.js
@@ -2,5 +2,7 @@ import { expect, test, vi } from "vitest";
 test("toReturnWith", () => {
   const mockFn = vi.fn(input => `mockFn(${input})`);
   mockFn("foo");
+  mockFn("bar");
   expect(mockFn).toReturnWith(`mockFn(foo)`);
+  expect(mockFn).toReturnWith(`mockFn(bar)`);
 });
