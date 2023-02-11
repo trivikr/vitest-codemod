b97dfb2be5b9a972f40d0fb83557fd5d5c4fcb84
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-11 13:03:30 -0800

    Add test with hint for expect/toThrowErrorMatchingSnapshot

--- packages/jest/src/__fixtures__/expect/toThrowErrorMatchingSnapshot.input.js
+++ packages/jest/src/__fixtures__/expect/toThrowErrorMatchingSnapshot.input.js
@@ -2,4 +2,8 @@ test("toThrowErrorMatchingSnapshot", () => {
   expect(() => {
     throw new Error("error");
   }).toThrowErrorMatchingSnapshot();
+
+  expect(() => {
+    throw new Error("error with hint");
+  }).toThrowErrorMatchingSnapshot("hint");
 });
--- packages/jest/src/__fixtures__/expect/toThrowErrorMatchingSnapshot.output.js
+++ packages/jest/src/__fixtures__/expect/toThrowErrorMatchingSnapshot.output.js
@@ -3,4 +3,8 @@ test("toThrowErrorMatchingSnapshot", () => {
   expect(() => {
     throw new Error("error");
   }).toThrowErrorMatchingSnapshot();
+
+  expect(() => {
+    throw new Error("error with hint");
+  }).toThrowErrorMatchingSnapshot("hint");
 });
