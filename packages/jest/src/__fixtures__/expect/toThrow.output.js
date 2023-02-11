12c41154b91f440710fc25fb967ea95d0d3f4726
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-11 12:54:15 -0800

    Add other test cases for expect/toThrow

--- packages/jest/src/__fixtures__/expect/toThrow.output.js
+++ packages/jest/src/__fixtures__/expect/toThrow.output.js
@@ -1,6 +1,24 @@
 import { expect, test } from "vitest";
 test("toThrow", () => {
-  expect(
-    () => { throw new Error("foo bar baz");}
-  ).toThrow("foo");
+  class CustomError extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "CustomError";
+    }
+  }
+
+  function throwFn() {
+    throw new CustomError("foo bar baz");
+  };
+  
+  // Test that the error message says "foo" somewhere: these are equivalent
+  expect(throwFn).toThrow(/foo/);
+  expect(throwFn).toThrow("foo");
+
+  // Test the exact error message
+  expect(throwFn).toThrow(/^foo bar baz$/);
+  expect(throwFn).toThrow(new Error("foo bar baz"));
+
+  // Test that we get a DisgustingFlavorError
+  expect(throwFn).toThrow(CustomError);
 });
