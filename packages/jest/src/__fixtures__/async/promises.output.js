4a2b4ca6f6caf480d8c413cf0358ee57f15f1be8
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-11 15:05:24 -0800

    Add test async/promises

---
+++ packages/jest/src/__fixtures__/async/promises.output.js
@@ -0,0 +1,36 @@
+import { describe, test, expect } from "vitest";
+describe("promises", () => {
+  describe("data", () => {
+    const mockData = "data";
+    const dataFn = () => Promise.resolve(mockData);
+
+    test("return", () => {
+      return dataFn().then((data) => {
+        expect(data).toBe(mockData);
+      });
+    });
+
+    test("await", async () => {
+      await dataFn().then((data) => {
+        expect(data).toBe(mockData);
+      });
+    });
+  });
+
+  describe("error", () => {
+    const mockError = new Error("error");
+    const dataFn = () => Promise.reject(mockError);
+
+    test("return", () => {
+      return dataFn().catch((error) => {
+        expect(error).toBe(mockError);
+      });
+    });
+
+    test("await", async () => {
+      await dataFn().catch((error) => {
+        expect(error).toBe(mockError);
+      });
+    });
+  });
+});
