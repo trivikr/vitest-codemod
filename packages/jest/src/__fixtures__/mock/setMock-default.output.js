600fb91239ecaa3a8408188963339b70c4d4a3a7
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-14 08:07:24 -0800

    Add test mock/setMock-default

---
+++ packages/jest/src/__fixtures__/mock/setMock-default.output.js
@@ -0,0 +1,9 @@
+import { expect, test, vi } from "vitest";
+test("setMock", async () => {
+  vi.mock("./defaultExport", () => ({
+    default: "defaultFromMock"
+  }));
+
+  const { default: defaultExport } = await import("./defaultExport");
+  expect(defaultExport).toBe("defaultFromMock");
+})
