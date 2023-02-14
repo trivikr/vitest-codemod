ba64e008f4215ddca5337118879789168d888178
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-13 21:43:12 -0800

    Revert "Add test mock/defaultExportEsm"
    This reverts commit 41183068807b858918d1c8e2d86d283bdf3d3614.

--- packages/jest/src/__fixtures__/mock/defaultExportEsm.input.js
+++
@@ -1,9 +0,0 @@
-jest.mock("./defaultExport", () => ({
-  __esModule: true,
-  default: "defaultFromMock"
-}));
-
-test("mockDefault", async () => {
-  const { default: defaultExport } = await import("./defaultExport");
-  expect(defaultExport).toBe("defaultFromMock");
-});
