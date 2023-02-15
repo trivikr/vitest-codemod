fec1f1ed4b3c2831e55af2fd6d48e119d1f798bc
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2023-02-15 08:27:31 -0800

    Add test for mock/doMock

---
+++ packages/jest/src/__fixtures__/mock/doMock.input.js
@@ -0,0 +1,22 @@
+beforeEach(() => {
+  // resetModules is required to reset mocks in jest.
+  // In vitest, however, doMock will override the previous mock.
+  jest.resetModules();
+});
+
+test('getToken 1', async () => {
+  jest.doMock('./getToken', () => ({
+    getToken: () => 'token1',
+  }));
+  const { getToken } = await import('./getToken');
+  expect(getToken()).toBe('token1');
+});
+
+test('getToken 2', async () => {
+  jest.doMock('./getToken', () => ({
+    getToken: () => 'token2',
+  }));
+  const { getToken } = await import('./getToken');
+  // ToDo: https://github.com/vitest-dev/vitest/issues/2870
+  // expect(getToken()).toBe('token2');
+});
